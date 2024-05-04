87eef84b78b295a12784eccffc2a76ebb55734c2
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2024-05-03 18:30:55 -0600

    pseudocoded gameplay functions

--- js/gameplay.js
+++ js/gameplay.js
@@ -5,14 +5,26 @@ const passTurn = () => {
 }
 
 const turnUp = () => {
-    //when currentPlayer
+    //For each player: 
+        //randomly select any of the player's cards.
+        //Randomly select another, except that it can't be +-4 (half of the starting size)
+        //If playing 12, pick a third, except that it can't be +-6 of either of the faceup cards
 }
 
-const draw = () => {
+const drawDeck = () => {
+    //Select the top card of the deck
+    //Render 2nd-from-top deck
+    //Animate it moving to the selected zone.
+    //turn it faceup
+}
+const drawDiscard = () => {
+    //Select top card of discard
+    //Render 2nd-from-top discard
+    //Animate it moving to the selected zone.
 
 }
 
 const play = () => {
-
+    //
 }
 
